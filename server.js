a491e34f08aca17c1948f0a47e4d6eb2af0e7fc8
Iansm21 <gw10smithian2@gmail.com>
2023-04-18 01:13:25 +0100

    Improved Filesystem

--- server.js
+++ server.js
@@ -1,42 +1,68 @@
+//Setup//--------------------------------------------------------------------------------
+//express setup-------------------------------------------------------------------------
 const express = require('express');
-const mongoose = require('mongoose');
-
-// Connect to Mongo Atlas database
-mongoose.connect('mongodb+srv://Admin:Admin@EnterpriseWebIS.mongodb.net/CourseworkIS?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
-
-// Create Express app
 const app = express();
 
-// Serve static files from the GitHub repository
-app.use(express.static('https://github.com/Iansm21/Enterprise-web-systems.git'));
+//sets up express session so that the user can remain signed in
+const session = require('express-session'); 
 
-// Define routes for web pages
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+//sets up the body parser
+const bodyParser = require('body-parser');
+
+app.use(express.urlencoded({extended: true}))
+
+//sets the view engine to ejs so we can make use of templates
+app.set('view engine', 'ejs');
+
+//allows the use of static elements
+app.use(express.static('public'));
+
+//variable to hold the database
+var db;
+
+app.use(session({
+    secret : 'example',
+    resave: true,
+    saveUninitialized: true
+  }));
+
+  
+//tells express we want to read posted forms
+app.use(bodyParser.urlencoded({
+    extended: true
+  }))
+
+app.use(bodyParser.json());
+
+app.use(function(req, res, next){
+  res.locals.username = req.session.currentuser
+  next();
+  })
+
+
+
+//MongoDB setup--------------------------------------------------------------------------------
+const mongoose = require('mongoose');
+
+const dbURI = process.env.MONGODB_URI || 'mongodb+srv://Admin:Admin@EnterpriseWebIS.mongodb.net/CourseworkIS?retryWrites=true&w=majority';
+
+mongoose.connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Connected to DB'))
+.catch(err => console.log(err));
+
+// Add error handling for MongoDB connection
+mongoose.connection.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
+    process.exit(-1);
 });
 
-app.get('/account', (req, res) => {
-  res.sendFile(__dirname + '/account.html');
-});
 
-// Define API routes for interacting with database
-app.get('/api/quotes', (req, res) => {
-  // TODO: Get all quotes from database and return as JSON
-});
 
-app.post('/api/quotes', (req, res) => {
-  // TODO: Create new quote in database and return as JSON
-});
-
-app.delete('/api/quotes/:id', (req, res) => {
-  // TODO: Delete quote with given ID from database
-});
-
-// Start server
-const PORT = process.env.PORT || 3000;
+// Start server--------------------------------------------------------------------------------
+const PORT = 8080;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+
