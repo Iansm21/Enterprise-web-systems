078895d17f02e7072c17788aeced2f7290ef79a9
Iansm21 <gw10smithian2@gmail.com>
2023-04-20 16:45:41 +0100

    Site finalized

--- public/js/NavScript.js
+++ public/js/NavScript.js
@@ -1,7 +1,7 @@
 //changes css. Gives width to sidenav class and changes body colour of site when navicon is pressed
 function openNav() {
     document.getElementById("nav").style.width = "200px";
-    document.body.style.backgroundColor = "rgba(58, 180, 160)";
+    document.body.style.backgroundColor = "rgb(58, 180, 160)";
   }
   //changes css. Gives width to sidenav class and changes body colour of site back when 'x' of sidenav is pressed
   function closeNav() {
