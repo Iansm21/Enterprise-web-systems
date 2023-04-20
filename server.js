3a6431f326523d555272811d1d95359e10d9be8a
Iansm21 <gw10smithian2@gmail.com>
2023-04-20 11:25:41 +0100

    port change

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ mongoose.connect(dbURI, {
 
 
 // Start server--------------------------------------------------------------------------------
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
